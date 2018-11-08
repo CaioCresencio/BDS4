0ee108bfb164e7610347a0e8e9cbedf77efb32ba
GabrielBergamim <gabrielbergamim@users.noreply.github.com>
2018-11-08 21:31:42 -0200

    Update estrutura.sql

--- estrutura.sql
+++ estrutura.sql
@@ -10,13 +10,19 @@ CREATE SEQUENCE seq_catLiteraria
 nocycle
 start with 1
 increment by 1
-maxvalue 99999;
+maxvalue 9999;
 
 CREATE TABLE categoria_literaria(
     codigo INT PRIMARY KEY,
     descricao VARCHAR2(70) NOT NULL,
     tempo_emprestimo INT NOT NULL
 );    
+
+CREATE SEQUENCE seq_obraLiteraria
+nocycle
+start with 1
+increment by 1
+maxvalue 9999;
 
 CREATE TABLE obra_literaria(
     id_obra INT PRIMARY KEY,
@@ -34,13 +40,25 @@ CREATE TABLE lista_autores(
     id_obra INT NOT NULL,
     FOREIGN KEY (id_obra) REFERENCES obra_literaria(id_obra)
 );
-    
+
+CREATE SEQUENCE seq_listaPalavras
+nocycle
+start with 1
+increment by 1
+maxvalue 9999;
+
 CREATE TABLE lista_palavras(
     id_lista INT PRIMARY KEY,
     id_obra INT NOT NULL,
     FOREIGN KEY (id_obra) REFERENCES obra_literaria(id_obra)
 );
-    
+
+CREATE SEQUENCE seq_palavraChave
+nocycle
+start with 1
+increment by 1
+maxvalue 9999;
+
 CREATE TABLE palavra_chave(
     id_palavra INT PRIMARY KEY,
     conteudo VARCHAR2(20)NOT NULL,
@@ -48,13 +66,25 @@ CREATE TABLE palavra_chave(
     FOREIGN KEY (id_lista) REFERENCES lista_palavras(id_lista)
 );
 
+CREATE SEQUENCE seq_autor
+nocycle
+start with 1
+increment by 1
+maxvalue 9999;
+
 CREATE TABLE autor(
     id_autor INT PRIMARY KEY,
     nome VARCHAR2(50) NOT NULL,
     id_lista INT NOT NULL,
     FOREIGN KEY (id_lista) REFERENCES lista_autores(id_lista)
 );
-    
+
+CREATE SEQUENCE seq_categoriaLeitor
+nocycle
+start with 1
+increment by 1
+maxvalue 9999;
+
 CREATE TABLE categoria_leitor(
     codigo_categoria INT PRIMARY KEY,
     descricao VARCHAR2(100) NOT NULL,
@@ -65,7 +95,13 @@ CREATE TABLE RG (
     numero INT PRIMARY KEY,
     estado INT NOT NULL
 );
-    
+
+CREATE SEQUENCE seq_leitor
+nocycle
+start with 1
+increment by 1
+maxvalue 9999;
+
 CREATE TABLE leitor(
     id_leitor INT PRIMARY KEY,
     nome VARCHAR2(100) NOT NULL,
@@ -77,14 +113,26 @@ CREATE TABLE leitor(
     rg INT NOT NULL,
     FOREIGN KEY (rg) REFERENCES rg(numero)
 );
-    
+
+CREATE SEQUENCE seq_exemplar
+nocycle
+start with 1
+increment by 1
+maxvalue 9999;
+
 CREATE TABLE exemplar(
     codigo_exemplar INT PRIMARY KEY,
     status INT NOT NULL,
     id_obra INT NOT NULL,
     FOREIGN KEY (id_obra) REFERENCES obra_literaria (id_obra)
 );
-    
+
+CREATE SEQUENCE seq_reserva
+nocycle
+start with 1
+increment by 1
+maxvalue 9999;
+
 CREATE TABLE reserva (
     codigo_reserva INT PRIMARY KEY,
     data_reserva DATE NOT NULL,
@@ -95,7 +143,13 @@ CREATE TABLE reserva (
     FOREIGN KEY (prontuario_func) REFERENCES funcionario(prontuario_func),
     FOREIGN KEY (id_leitor) REFERENCES leitor(id_leitor)
 ); 
-    
+
+CREATE SEQUENCE seq_emprestimo
+nocycle
+start with 1
+increment by 1
+maxvalue 9999;
+
 CREATE TABLE emprestimo (
     codigo_emp INT PRIMARY KEY,
     data_dev DATE NOT NULL,
