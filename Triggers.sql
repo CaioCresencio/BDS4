ef0d455c5aa0039564a300194ca48d619ca1d527
GabrielBergamim <gabrielbergamim@users.noreply.github.com>
2018-11-10 18:21:14 -0200

    Update Triggers.sql

---
+++ Triggers.sql
@@ -0,0 +1,10 @@
+--dropar a base porque mudei os status do exemplar para string (disponivel, emprestado);
+CREATE OR REPLACE TRIGGER cadastra_exemplar
+    AFTER INSERT ON obra_literaria FOR EACH ROW
+BEGIN
+    FOR i IN 1..:NEW.qtd_exemplares LOOP
+        INSERT INTO exemplar VALUES(seq_exemplar.nextval, 'DISPONÍVEL', :NEW.id_obra);
+    END LOOP;
+END;
+/
+
